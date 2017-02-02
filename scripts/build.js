63c104aa1f87bb6ee3fef8c79b06ab37f06aff17
sh4869 <nobuk4869@gmail.com>
2017-02-02 17:19:16 +0900

    Create Simple Generator

---
+++ scripts/build.js
@@ -0,0 +1,17 @@
+const fs = require('fs-extra');
+const pug = require("pug");
+
+const copySlides = () => {
+    fs.copySync(__dirname + "/../slides/", __dirname + "/../dest/");
+}
+
+const generateIndexPage = () => {
+    fs.copySync(__dirname + "/../style.css", __dirname + "/../dest/style.css");
+    const info = JSON.parse(fs.readFileSync(__dirname + "/../slides.json"));
+    const value = pug.compileFile(__dirname + "/../index.pug", { pretty: true })({ slides: info });
+    fs.writeFileSync(__dirname + "/../dest/index.html", value);
+}
+
+copySlides();
+
+generateIndexPage();
---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+const ghpages = require("gh-pages");
+const path = require("path");
+
+const GH_TOKEN = process.env.GH_TOKEN;
+
+ghpages.publish(
+    path.join(__dirname, '..', 'dest'),
+    {
+        repo: 'https://' + GH_TOKEN + '@github.com/sh4869/slides',
+        user: {
+            name: "sh4869",
+            email: "nobuk4869@gmail.com",
+        },
+        message: "[ci skip] UPDATEE WEBPAGE",
+        branch: "master"
+    },
+    function (err) {
+        if(err){
+            console.log(err);
+            process.exit(1);
+        } else {
+            console.log("Update WebPage");
+        }
+    }
+);
