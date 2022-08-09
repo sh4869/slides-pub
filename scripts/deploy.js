166f1657bdea1b0bc61e0883d765a34a2d266884
sh4869 <nobuk4869@gmail.com>
2022-08-10 00:30:26 +0900

    update packages

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,22 +4,23 @@ const path = require("path");
 const GH_TOKEN = process.env.GH_TOKEN;
 
 ghpages.publish(
-    path.join(__dirname, '..', 'dest'),
-    {
-        repo: 'https://' + GH_TOKEN + '@github.com/sh4869/slides',
-        user: {
-            name: "sh4869",
-            email: "nobuk4869@gmail.com",
-        },
-        message: "[ci skip] UPDATEE WEBPAGE",
-        branch: "gh-pages"
+  path.join(__dirname, "..", "dest"),
+  {
+    repo: "https://" + GH_TOKEN + "@github.com/sh4869/slides-pub",
+    user: {
+      name: "sh4869",
+      email: "nobuk4869@gmail.com",
     },
-    function (err) {
-        if(err){
-            console.log(err);
-            process.exit(1);
-        } else {
-            console.log("Update WebPage");
-        }
+    message: "[ci skip] UPDATEE WEBPAGE",
+    branch: "gh-pages",
+    history: true
+  },
+  function (err) {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    } else {
+      console.log("Update WebPage");
     }
+  }
 );
